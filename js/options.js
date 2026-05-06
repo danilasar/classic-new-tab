404ba6df1251c839c84ef6e178cd71ca5e533c76
Данила Григорьев <danila.sar@yandex.ru>
2026-05-07 01:23:09 +0400

    Revert "feat: вернул старые настройки"
    This reverts commit 29c7f9fee8dd2d28c5b1690894f1dd6741639500.

--- js/options.js
+++ js/options.js
@@ -4,55 +4,12 @@
 var HIDDEN_TOP_SITES_KEY = 'old_ntp.hidden_top_sites';
 var PINNED_TOP_SITES_KEY = 'old_ntp.pinned_top_sites';
 var TOP_SITE_PREVIEWS_KEY = 'old_ntp.top_site_previews';
-var LEGACY_KEYS = ['syncOptions', 'showWelcome', 'url'];
 
 var state = {
     hiddenTopSites: [],
     pinnedTopSites: [],
     previews: {},
-    legacyOptions: {
-        syncOptions: false,
-        showWelcome: true,
-        url: ''
-    },
     editingIndex: -1
-};
-
-var legacyCustomPages = {
-    'New Tab Redirect Apps': ''
-};
-
-var legacyChromePages = {
-    Apps: 'chrome://apps/',
-    Extensions: 'chrome://extensions/',
-    History: 'chrome://history/',
-    Downloads: 'chrome://downloads/',
-    Bookmarks: 'chrome://bookmarks/',
-    Internals: 'chrome://net-internals'
-};
-
-var legacyAboutPages = {
-    'about:blank': 'about:blank',
-    'about:version': 'about:version',
-    'about:plugins': 'about:plugins',
-    'about:cache': 'about:cache',
-    'about:memory': 'about:memory',
-    'about:histograms': 'about:histograms',
-    'about:dns': 'about:dns',
-    'about:terms': 'about:terms',
-    'about:credits': 'about:credits',
-    'about:net-internals': 'about:net-internals'
-};
-
-var legacyPopularPages = {
-    'Google+': 'https://plus.google.com',
-    Facebook: 'https://www.facebook.com',
-    Twitter: 'https://twitter.com',
-    Yahoo: 'https://www.yahoo.com',
-    Wikipedia: 'http://www.wikipedia.org',
-    Digg: 'http://digg.com',
-    Delicious: 'https://delicious.com',
-    Slashdot: 'http://www.slashdot.org'
 };
 
 function msg(key) {
@@ -107,54 +64,6 @@ function topSiteKey(url) {
     return normalizeUrl(url);
 }
 
-function legacyUrlPasses(url) {
-    var protocols = [
-        'http://',
-        'https://',
-        'about:',
-        'file://',
-        'file:\\',
-        'file:///',
-        'chrome://',
-        'chrome-internal://',
-        'chrome-extension://'
-    ];
-    var i;
-
-    if(!url) {
-        return true;
-    }
-
-    for(i = 0; i < protocols.length; i++) {
-        if(url.indexOf(protocols[i]) === 0) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-function normalizeLegacyUrl(url) {
-    var parts;
-
-    url = (url || '').trim();
-
-    if(!url) {
-        return '';
-    }
-
-    if(legacyUrlPasses(url) && url.length > 8) {
-        return url;
-    }
-
-    parts = url.split('://');
-    if(parts.length > 1) {
-        return 'http://' + parts[1];
-    }
-
-    return 'http://' + url;
-}
-
 function showStatus(message) {
     var status = document.getElementById('status');
 
@@ -204,41 +113,6 @@ function renderPreviewCount() {
     text(document.getElementById('previewCount'),
          chrome.i18n.getMessage('settingsPreviewCount', [String(count)]) ||
          String(count));
-}
-
-function renderLegacyOptions() {
-    document.getElementById('legacyUrl').value = state.legacyOptions.url || '';
-    document.getElementById('legacySync').checked =
-        state.legacyOptions.syncOptions !== undefined ?
-            !!state.legacyOptions.syncOptions : false;
-    document.getElementById('legacyShowWelcome').checked =
-        state.legacyOptions.showWelcome !== undefined ?
-            !!state.legacyOptions.showWelcome : true;
-}
-
-function createQuickLink(label, url) {
-    return createButton(label || msg('settingsOriginalNewTab'), 'secondary-button',
-        function() {
-            document.getElementById('legacyUrl').value = url || '';
-            saveLegacyOptions();
-        });
-}
-
-function renderQuickLinks(containerId, pages) {
-    var container = document.getElementById(containerId);
-
-    clearNode(container);
-
-    Object.keys(pages).forEach(function(label) {
-        container.appendChild(createQuickLink(label, pages[label]));
-    });
-}
-
-function renderLegacyQuickLinks() {
-    renderQuickLinks('legacyCustomPages', legacyCustomPages);
-    renderQuickLinks('legacyChromePages', legacyChromePages);
-    renderQuickLinks('legacyAboutPages', legacyAboutPages);
-    renderQuickLinks('legacyPopularPages', legacyPopularPages);
 }
 
 function createButton(label, className, callback) {
@@ -327,8 +201,6 @@ function renderHiddenTopSites() {
 }
 
 function render() {
-    renderLegacyOptions();
-    renderLegacyQuickLinks();
     renderPinnedTopSites();
     renderHiddenTopSites();
     renderPreviewCount();
@@ -450,98 +322,19 @@ function clearPreviews() {
     });
 }
 
-function readLegacyOptionsFromForm() {
-    return {
-        url: normalizeLegacyUrl(document.getElementById('legacyUrl').value),
-        syncOptions: document.getElementById('legacySync').checked,
-        showWelcome: document.getElementById('legacyShowWelcome').checked
-    };
-}
-
-function saveLegacyOptions(event) {
-    var values;
-
-    if(event) {
-        event.preventDefault();
-    }
-
-    values = readLegacyOptionsFromForm();
-    state.legacyOptions = values;
-
-    storageSet('local', values, function() {
-        if(values.syncOptions) {
-            storageSet('sync', values, function() {
-                renderLegacyOptions();
-                showStatus(msg('settingsSaved'));
-            });
-            return;
-        }
-
-        renderLegacyOptions();
-        showStatus(msg('settingsSaved'));
-    });
-}
-
-function restoreLegacyOptions() {
-    storageGet('local', LEGACY_KEYS, function(items) {
-        state.legacyOptions = {
-            url: items.url || '',
-            syncOptions: items.syncOptions !== undefined ?
-                !!items.syncOptions : false,
-            showWelcome: items.showWelcome !== undefined ?
-                !!items.showWelcome : true
-        };
-        renderLegacyOptions();
-    });
-}
-
-function loadLegacySyncOptions() {
-    storageGet('sync', LEGACY_KEYS, function(items) {
-        if(items.url !== undefined) {
-            state.legacyOptions = {
-                url: items.url || '',
-                syncOptions: true,
-                showWelcome: items.showWelcome !== undefined ?
-                    !!items.showWelcome : true
-            };
-            renderLegacyOptions();
-            saveLegacyOptions();
-        }
-    });
-}
-
 function loadState(callback) {
     storageGet('sync', [HIDDEN_TOP_SITES_KEY, PINNED_TOP_SITES_KEY],
         function(syncItems) {
             storageGet('local', [TOP_SITE_PREVIEWS_KEY], function(localItems) {
-                storageGet('local', LEGACY_KEYS, function(legacyItems) {
-                    state.hiddenTopSites = syncItems[HIDDEN_TOP_SITES_KEY] || [];
-                    state.pinnedTopSites = syncItems[PINNED_TOP_SITES_KEY] || [];
-                    state.previews = localItems[TOP_SITE_PREVIEWS_KEY] || {};
-                    state.legacyOptions = {
-                        url: legacyItems.url || '',
-                        syncOptions: legacyItems.syncOptions !== undefined ?
-                            !!legacyItems.syncOptions : false,
-                        showWelcome: legacyItems.showWelcome !== undefined ?
-                            !!legacyItems.showWelcome : true
-                    };
-                    callback();
-                });
+                state.hiddenTopSites = syncItems[HIDDEN_TOP_SITES_KEY] || [];
+                state.pinnedTopSites = syncItems[PINNED_TOP_SITES_KEY] || [];
+                state.previews = localItems[TOP_SITE_PREVIEWS_KEY] || {};
+                callback();
             });
         });
 }
 
 function bindEvents() {
-    document.getElementById('legacyForm')
-        .addEventListener('submit', saveLegacyOptions);
-    document.getElementById('legacyCancel')
-        .addEventListener('click', restoreLegacyOptions);
-    document.getElementById('legacyLoadSync')
-        .addEventListener('click', loadLegacySyncOptions);
-    document.getElementById('legacySync')
-        .addEventListener('change', saveLegacyOptions);
-    document.getElementById('legacyShowWelcome')
-        .addEventListener('change', saveLegacyOptions);
     document.getElementById('addTile').addEventListener('click', function() {
         openTileForm(-1);
     });
